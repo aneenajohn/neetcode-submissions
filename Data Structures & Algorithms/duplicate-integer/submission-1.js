37baa0c3905f9f5448e1ea506c68d2791090a4ba
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2026-04-26 23:52:11 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const seen = new Set();
+        for(let num of nums){
+            if(seen.has(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+        // Approach with hash map(but you dont need to know the freq of repeat)
+        // let numsMap = new Map()
+        // for(let num in nums){
+        //     if(numsMap.has(nums[num])) {
+        //         numsMap.set(nums[num], (numsMap.get(nums[num]) + 1));
+        //         return true
+        //     }else {
+        //         numsMap.set(nums[num], 1);
+        //     }
+        // }
+        // console.log(numsMap)
+        // return false;
+    }
+}
